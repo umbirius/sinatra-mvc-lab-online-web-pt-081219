a78a3723925cc1b4962bc627e79cc82325a1f2f2
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-14 15:28:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input 
+  end 
+  
+  post '/piglatinize' do 
+    @piglatin = PigLatinizer.new
+    @pigtalk = @piglatin.piglatinize(params[:string])
+    erb :final_view
+  end 
 end
